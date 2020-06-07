f568188ce566d57ae9326f488e0e455b3a0d0a0c
Josef Kousal <pepa.kousal@gmail.com>
2020-06-07 21:11:05 +0200

    Added some classes

--- src/main.java
+++ src/main.java
@@ -1,14 +1,26 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class main {
 
-    public static void main(String []args){
+    public static void main(String[] args) {
         System.out.println("Dobry den");
+
+        ArrayList<String> subjects = new ArrayList<>(Arrays.asList("Math", "History", "Physics"));
+        Teacher teacher1 = new Teacher("Bob", 41, subjects);
+        Student student1 = new Student("Alice", 3, subjects);
+
+        teacher1.setAge(55);
+        teacher1.setName("Gayboy");
+        student1.setGrade(2);
+        student1.setName("Karel");
 
         int b = 4;
         int vypocet;
         vypocet = soucet(b);
 
         int cyklus = 0;
-        for (int i = 1; i<10; i++){
+        for (int i = 1; i < 10; i++) {
             cyklus += i;
         }
         System.out.println(cyklus);
