3e544bceee420bb3ebea7542802f52241e2d9851
OndrejPavlas <ondrej.pavlas1@tul.cz>
2020-06-07 20:52:17 +0200

    testuji

---
+++ src/main.java
@@ -0,0 +1,17 @@
+public class main {
+
+    public static void main(String []args){
+        System.out.println("Dobry den");
+
+        int b = 4;
+        int vypocet;
+        vypocet = soucet(b);
+
+        for (int i = 1; )
+    }
+
+    private static int soucet(int b) {
+        int a = 5;
+        return a + b;
+    }
+}
